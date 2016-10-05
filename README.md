3d036833652421d97c79a9d1c81df100ac8373bf
Guglio <guglio95@users.noreply.github.com>
2016-10-05 22:46:35 +0200

    fixati numerosi bugs

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Interprete per il linguaggio della macchina di <b>Von Neumann.</b><br/>
 Il backend è stato scritto in <b>PHP</b>,<br/>
-Il frontend è scritto utilizzando <b>Bootstrap, JQuery e Ace Editor </b> (syntax highlighter)<br/>
+Il frontend è scritto utilizzando <b>Bootstrap, JQuery, jquery-confirm e Ace Editor </b> (syntax highlighter)<br/>
 Versione online su <a href="http://vonsim.guglio.net">http://vonsim.guglio.net</a>
--- static/js/custom.js
+++ static/js/custom.js
@@ -1,110 +1,236 @@
+//Variabili globali
 var timer;//Timer verso l'esecuzione della prox riga
 var NextLine;//Prossima da eseguire
 var ExecLine;//Correntemente in esecuzione
 var UploadNeeded = true;//devo caricare il codice sul server
-
-$(document).ready(function() {
-	$("#Memoria").hide();//@TODO Bisognerebbe metterlo nel css...
-
-	$( "#esegui" ).click(function() {
-			loadCode();
-			timer = setTimeout(function(){ exec(true); }, 1000);
-	});
-
-});
-
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/twilight");
 editor.session.setMode("ace/mode/ruby");
 editor.setOption("firstLineNumber", parseInt(0));
 editor.setFontSize(23); // will set font-size: 10px
-editor.on("change", function(){ UploadNeeded = true; });//Se il codice viene alterato va ricaricato
+editor.on("change", function () {
+    UploadNeeded = true;
+});//Se il codice viene alterato va ricaricato
 
-function loadCode(){
-	if (UploadNeeded){
-	var code = editor.getValue();
-	$.post( "./server.php", { code: code })
-	.done(function( data ) {
-		//alert("Listato caricato");
-		$("#Memoria_tbody").empty();
-	});
+
+$(document).ready(function () {
+    $("#Memoria").hide();//@TODO Bisognerebbe metterlo nel css...
+
+    if(/chrom(e|ium)/.test(navigator.userAgent.toLowerCase())){
+        intro();
+    }else{
+        alert("Ciao, ti consiglio di utilizzare il sito dal PC utilizzando GoogleChrome");
+    }
+    //Gestione pulsantoni:
+    $("#esegui").click(function () {
+        clearInterval(timer);
+        loadCode();
+        timer = setInterval(function () {
+            exec(true);
+        }, 1000);
+    });
+
+    $("#esegui_step").click(function () {
+        clearInterval(timer);
+        loadCode();
+        exec(false);
+    });
+
+});
+
+/**
+ * Carica il codice sul server se non è aggiornato
+ */
+function loadCode() {
+    if (UploadNeeded) {
+        UploadNeeded = false;
+        var code = editor.getValue();
+        $.post("./server.php", {code: code})
+            .done(function (data) {
+                //alert("Listato caricato");
+                $("#Memoria_tbody").empty();
+            });
+    }
 }
-function exec(topBottom){
-	if (NextLine >= 0) {
-		selectLine(NextLine);
-	}
-	$.getJSON( "./server.php", { exec: "1" } )
-	.done(function( json ) {
-		if (json.result == "OK"){//Il server risponde OK
-			ExecLine = json.message.ExecLine;
-			NextLine = json.message.NextLine;
-			var Asking_Data = json.message.Asking_Data;
-			var Accumulatore = json.message.Accumulatore;
-			var NastroOut = json.message.NastroOut;
-			var Memoria = json.message.Memoria;
 
-			if (ExecLine >= 0) {
-				selectLine(ExecLine);
+/**
+ * Fa avanzare l'esecuzione
+ * @boolean topBottom: identifica se l'esecuzione è stepbystep
+ */
+function exec(topBottom) {//topBottom
+    if (NextLine >= 0) {
+        selectLine(NextLine);//Seleziono la riga che sto x eseguire
+    }
+    $.getJSON("./server.php", {exec: "1"})
+        .done(function (json) {
+            if (json.result == "OK") {//Il server risponde OK
+                ExecLine = json.message.ExecLine;
+                NextLine = json.message.NextLine;
+                var Asking_Data = json.message.Asking_Data;
+                var Accumulatore = json.message.Accumulatore;
+                var NastroOut = json.message.NastroOut;
+                var Memoria = json.message.Memoria;
 
-				if (ExecLine != NextLine && topBottom == true){//Se non siamo in loop continuo l'esecuzione (La READ è considata un LOOP)
-					timer = setTimeout(function(){ exec(); }, 1000);//Schedulo l'esecuzione della riga successiva
-				}
-			}
-			setAccumulatore(Accumulatore);
-			if (NastroOut != null){setNastroOut(NastroOut); }
-			setMemoria(Memoria);
-			if (ExecLine == -1){
-				alert("Programma terminato");
-			}
-			if (Asking_Data){
-				var data = null;
-				while (data==null){
-					data = prompt("Il listato chiede un valore", "57");
-				}
-				$.getJSON( "./server.php", { NastroIn: data } )
-				.done(function( json ) {
-					if (json.result == "OK"){
-						timer = setTimeout(function(){ exec(); }, 1000);//Riprendo l'esecuzione
-					}else alert(json.message);
-				});
+                //Aggiorno la grafica
+                setAccumulatore(Accumulatore);
+                if (NastroOut != null) {
+                    setNastroOut(NastroOut);
+                }
 
-			}
-		}else alert(json.message);
-	})
-	.fail(function( jqxhr, textStatus, error ) {//Il client non connette
-		var err = textStatus + ", " + error;
-		alert( "Request Failed: " + err );
-	});
+                setMemoria(Memoria);
+
+                if (ExecLine == -1) {//L'istruzione END è stata eseguita
+                    clearInterval(timer);
+                    fine_listato();
+                }
+
+                if (ExecLine >= 0) {
+                    selectLine(ExecLine);
+                    if (ExecLine == NextLine) {//Se siamo in loop o in attesa di una READ blocco tutto
+                        clearInterval(timer);
+                    }
+                }
+
+                if (Asking_Data) {//Se il server richiede dei dati mostro un popup
+                    clearInterval(timer);
+                    var data = null;
+                    $.confirm({
+                        title: 'Natro in Ingresso',
+                        keyboardEnabled: true,
+                        confirmButton: 'Invia',
+                        confirmButtonClass: 'btn-success',
+                        content: 'url:./static/html/NastroIn.html',
+                        confirm: function () {
+                            var input = this.$b.find('input#input-modal');
+                            var errorText = this.$b.find('.text-danger');
+                            if (input.val() == '') {
+                                errorText.show();
+                                return false;
+                            } else {
+                                //Se ho un dato presunto valido lo comunico al serben
+                                data = input.val();
+                                setAccumulatore(data);
+                                $.getJSON("./server.php", {NastroIn: data})
+                                    .done(function (json) {
+                                        if (json.result == "OK") {
+                                            if (topBottom) {//Se non siamo in step-by-step bisogna riavviare il timer
+                                                timer = setInterval(function () {
+                                                    exec(true);
+                                                }, 1000);
+                                            }
+                                        } else errore(json.message);
+                                    });
+                            }
+                        }
+                    });
+                }
+            } else errore(json.message);
+        })
+        .fail(function (jqxhr, textStatus, error) {//Il client non connette
+            var err = textStatus + ", " + error;
+            errore("Request Failed: " + err);
+        });
 }
-function selectLine(line){
-	editor.selection.moveCursorToPosition({row: line, column: 0});
-	editor.selection.selectLine();
-}
-function setAccumulatore(value){
-	$('#Accumulatore').html(value);
-}
-function setNastroOut(value){
-	$('#NastroOut').html(value);
-}
-function setMemoria(memoria){
-	$("#MemoriaPH").hide();
-	$("#Memoria").show();
 
-	$.each( memoria, function( key, value ){
-		//Verifico se la cella di memoria esiste già nell'html
-		if ( $( "#memoria_" + key ).length ) {
-			//Esiste, controllo se il contenuto è cambiato
-			var valore_attuale = ($( "#memoria_" + key + " td:nth-child(2)").html());
-			if (value == valore_attuale){
-				$( "#memoria_" + key ).removeClass( "danger" );//Se non è cambiato tolgo il rosso.
-			}else{
-				$( "#memoria_" + key + " td:nth-child(2)").html(value);//Se è cambiato aggiorno
-				$( "#memoria_" + key ).addClass( "danger" );//Se è cambiato metto il rosso.
-			}
-		}else{
-			//Non esiste, quindi la creo
-			$('#Memoria > tbody:last-child').append('<tr id="memoria_' + key + '"class="danger"><td class="col-xs-4">'+ key +'</td><td class="col-xs-8">'+ value +'</td></tr>');
-		}
-	});
+/**
+ * Seleziona la linea nel box di testo
+ * @param line
+ */
+function selectLine(line) {
+    editor.selection.moveCursorToPosition({row: line, column: 0});
+    editor.selection.selectLine();
+}
 
+/**
+ * Aggiorna l'accumulatore
+ * @param value
+ */
+function setAccumulatore(value) {
+    $('#Accumulatore').html(value);
+}
+
+/**
+ * Aggiorna il nastro d'uscita
+ * @param value
+ */
+function setNastroOut(value) {
+    $('#NastroOut').html(value);
+}
+
+/**
+ * Aggiorna la tabella della memoria
+ * @param memoria
+ */
+function setMemoria(memoria) {
+    $("#MemoriaPH").hide();
+    $("#Memoria").show();
+
+    $.each(memoria, function (key, value) {
+        //Verifico se la cella di memoria esiste già nell'html
+        if ($("#memoria_" + key).length) {
+            //Esiste, controllo se il contenuto è cambiato
+            var valore_attuale = ($("#memoria_" + key + " td:nth-child(2)").html());
+            if (value == valore_attuale) {
+                $("#memoria_" + key).removeClass("danger");//Se non è cambiato tolgo il rosso.
+            } else {
+                $("#memoria_" + key + " td:nth-child(2)").html(value);//Se è cambiato aggiorno
+                $("#memoria_" + key).addClass("danger");//Se è cambiato metto il rosso.
+            }
+        } else {
+            //Non esiste, quindi la creo
+            $('#Memoria > tbody:last-child').append('<tr id="memoria_' + key + '"class="danger"><td class="col-xs-4">' + key + '</td><td class="col-xs-8">' + value + '</td></tr>');
+        }
+    });
+
+}
+
+/**
+ * Mostra a video un messaggio d'errore e resetta i timers
+ * @param msg
+ */
+function errore(msg) {
+    clearInterval(timer);
+    $.alert({
+        title: 'Ops...',
+        content: msg,
+        confirmButton: 'Okay',
+        confirmButtonClass: 'btn-primary',
+        icon: 'fa fa-info',
+        animation: 'zoom'
+    });
+}
+
+/**
+ * A fine listato chiede se voglio resettare la MdV
+ */
+function fine_listato() {
+    $.confirm({
+        title: 'Perfetto!',
+        content: 'Il listato &egrave; stato intepretato con successo,<br/>Se vuoi eseguire nuovamente il codice, resetta la macchina.',
+        confirmButton: 'Resetta Macchina',
+        confirmButtonClass: 'btn-danger',
+        cancelButton: 'Ok',
+        cancelButtonClass: 'btn-info',
+        icon: 'fa fa-question-circle',
+        animation: 'scale',
+        animationClose: 'top',
+        opacity: 0.5,
+        confirm: function () {
+            UploadNeeded = true;
+        }
+    });
+}
+
+/**
+ * Breve intro all'apertura
+ */
+function intro(){
+    clearInterval(timer);
+    $.alert({
+        title: 'Ciao',
+        content: 'Questo simulatore della Macchina di VonNeumann &egrave; del tutto sperimentale, <u>se qualcosa non funziona segnalamelo</u>',
+        confirmButton: 'Capito',
+        confirmButtonClass: 'btn-success',
+        icon: 'fa fa-info'
+    });
 }
